66705d9749907ecd69394944ab604dd4c3722700
Mohit Sojitra <mohitsojitra0@gmail.com>
2021-09-29 17:31:34 +0530

    username and password added

--- src/Screens/Signin.js
+++ src/Screens/Signin.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react'
 import {
   Box,
   Avatar,
@@ -14,54 +14,59 @@ import {
   createTheme,
   ThemeProvider,
   makeStyles,
-} from "@mui/material";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { Api } from "../utils/API";
-import { toast } from "react-toastify";
-import { setCregential } from "../utils/localstorage";
-import { useHistory } from "react-router-dom";
-import { pagePath } from "../utils/config";
-import { colors } from "../Theme/ColorPalette";
+} from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import {Api} from '../utils/API'
+import {toast} from 'react-toastify'
+import {setCregential} from '../utils/localstorage'
+import {useHistory} from 'react-router-dom'
+import {pagePath} from '../utils/config'
+import {colors} from '../Theme/ColorPalette'
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {"Copyright © "}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {'Copyright © '}
       <Link color="inherit" href="#">
         Your Website
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
-  );
+  )
 }
 
-const theme = createTheme();
+const theme = createTheme()
 
 export default function SignIn() {
-  const history = useHistory();
+  const history = useHistory()
   const handleSubmit = React.useCallback(
-    async (event) => {
-      event.preventDefault();
-      const input = new FormData(event.currentTarget);
+    async event => {
+      event.preventDefault()
+      const input = new FormData(event.currentTarget)
       // eslint-disable-next-line no-console
       const cred = {
-        userName: btoa(input.get("userName")),
-        password: btoa(input.get("password")),
-      };
-
-      const { statusCode, data } = await Api.postRequest("/auth/signin", cred);
-      const { isOk, message } = JSON.parse(data);
-      if (statusCode === 400 || statusCode === 500 || !isOk) {
-        toast.error(message);
-        return;
+        userName: btoa(input.get('userName')),
+        password: btoa(input.get('password')),
       }
-      toast.success(message);
-      setCregential(cred);
-      history.replace(pagePath.ARTIST_PAGE);
+
+      const {statusCode, data} = await Api.postRequest('/auth/signin', cred)
+      const {isOk, message} = JSON.parse(data)
+      if (statusCode === 400 || statusCode === 500 || !isOk) {
+        toast.error(message)
+        return
+      }
+      toast.success(message)
+      setCregential(cred)
+      history.replace(pagePath.ARTIST_PAGE)
     },
-    [history]
-  );
+    [history],
+  )
 
   return (
     <ThemeProvider theme={theme}>
@@ -70,19 +75,29 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, background: colors.primary }}>
+          <Avatar sx={{m: 1, background: colors.primary}}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField margin="normal" required fullWidth id="userName" label="User name" name="userName" autoComplete="userName" autoFocus />
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+            <TextField
+              value={'artistee@username'}
+              margin="normal"
+              required
+              fullWidth
+              id="userName"
+              label="User name"
+              name="userName"
+              autoComplete="userName"
+              autoFocus
+            />
             <TextField
               margin="normal"
               required
@@ -92,12 +107,18 @@ export default function SignIn() {
               type="password"
               id="password"
               autoComplete="current-password"
+              value={'artistee@password'}
             />
             {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2, background: colors.primary }}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{mt: 3, mb: 2, background: colors.primary}}
+            >
               Sign In
             </Button>
             {/* <Grid container>
@@ -117,5 +138,5 @@ export default function SignIn() {
         {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
       </Container>
     </ThemeProvider>
-  );
+  )
 }
